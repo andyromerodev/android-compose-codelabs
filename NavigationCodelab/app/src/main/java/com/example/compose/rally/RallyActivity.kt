dc5abeacbd7c6deda78668107d20a88470f27664
Andy Romero <andy910304@gmail.com>
2023-05-04 11:34:51 -0400

    navigate to destination

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -56,7 +56,8 @@ fun RallyApp() {
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { newScreen ->
+                        navController.navigate(newScreen.route) },
                     currentScreen = currentScreen
                 )
             }
